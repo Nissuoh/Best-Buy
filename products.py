8539345329633b40fd64b2b8d7826872a82350fa
Houssin <el-hb@outlook.de>
2026-01-25 18:00:54 +0100

    jetzt wurden die funktionen definiert und ausgearbeitet und auch getestet

--- products.py
+++ products.py
@@ -1,39 +1,50 @@
-from dataclasses import dataclass
-
-
 class Product:
+    def __init__(self, name, price, quantity):
+        self.name: str = str(name)
+        self.price: float = float(price)
+        self.quantity: int = int(quantity)
+        self.active: bool = True
 
-    @dataclass
-    def __init__(self):
-        name: str
-        price: float
-        quantity: int
-        active: True
+        if self.quantity == 0:
+            self.active = False
 
+    def get_quantity(self) -> int:
+        return self.quantity
 
-def get_quantity(self) -> int:
-    pass
+    def set_quantity(self, quantity: int):
+        if quantity < 0:
+            raise ValueError("quantity must be >= 0")
 
+        self.quantity = int(quantity)
 
-def set_quantity(self, quantity):
-    pass
+        if self.quantity == 0:
+            self.deactivate()
+        else:
+            self.active = True
 
+    def is_active(self) -> bool:
+        return self.active
 
-def is_active(self) -> bool:
-    pass
+    def activate(self):
+        self.active = True
 
+    def deactivate(self):
+        self.active = False
 
-def activate(self):
-    pass
+    def show(self):
+        print(f"{self.name}, Price: {self.price}, Quantity: {self.quantity}")
 
+    def buy(self, quantity: int) -> float:
+        if quantity <= 0:
+            raise ValueError("quantity to buy must be > 0")
+        if not self.active:
+            raise ValueError("product is inactive")
+        if quantity > self.quantity:
+            raise ValueError("not enough stock")
 
-def deactivate(self):
-    pass
+        self.quantity -= int(quantity)
 
+        if self.quantity == 0:
+            self.deactivate()
 
-def show(self):
-    pass
-
-
-def buy(self, quantity) -> float:
-    pass
+        return float(quantity) * self.price
